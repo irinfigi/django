6073ba3823d7f38093706d2b594b6547a507196e
Irin F Mampilly <irinfigi123@gmail.com>
2024-04-23 23:50:40 +0530

    modified:   hello_django/hello_app/__pycache__/views.cpython-312.pyc 	modified:   hello_django/hello_app/views.py 	modified:   hello_django/hello_django/__pycache__/settings.cpython-312.pyc 	modified:   hello_django/hello_django/settings.py 	new file:   hello_django/templates/hello.html

--- hello_django/hello_app/views.py
+++ hello_django/hello_app/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def print_hello(request):
-    return HttpResponse("hello django")
+    movie_data={
+        'movies' : [{
+        'title':'Godfather',
+        'year':1990,
+        'summary':'story of an untold kin',
+        'sucess':True    
+        },
+        {
+        'title':'Titanic',
+        'year':1990,
+        'summary':'Love story',
+        'sucess':False    
+        },
+        {
+        'title':'Kingkong',
+        'year':1990,
+        'summary':'story of an ape',
+        'sucess':True 
+        }   
+    ]}
+    return render(request,'hello.html',movie_data)
