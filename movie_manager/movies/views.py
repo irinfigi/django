c2a49c341cf0d3c3d513b834f7bb3ba01c4824c8
Irin F Mampilly <irinfigi123@gmail.com>
2024-04-25 15:57:15 +0530

    Changes to be committed: 	modified:   movie_manager/movies/__pycache__/views.cpython-312.pyc 	modified:   movie_manager/movies/views.py 	modified:   movie_manager/templates/list.html

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -6,7 +6,27 @@ def create(request):
     return render(request,'create.html')
 
 def list(request):
-    return render(request,'list.html')
+    movie_data={
+        'movies' : [{
+        'title':'Godfather',
+        'year':1990,
+        'summary':'story of an untold kin',
+        'sucess':True    
+        },
+        {
+        'title':'Titanic',
+        'year':1990,
+        'summary':'Love story',
+        'sucess':False    
+        },
+        {
+        'title':'Kingkong',
+        'year':1990,
+        'summary':'story of an ape',
+        'sucess':True 
+        }   
+    ]}
+    return render(request,'list.html',movie_data)
 
 def edit(request):
     return render(request,'edit.html')
