d6e97884ee7499a71d45247d3fc1ede4a94771ec
Irin F Mampilly <irinfigi123@gmail.com>
2024-04-27 03:21:39 +0530

    modified:   movie_manager/movies/__pycache__/models.cpython-312.pyc 	new file:   movie_manager/movies/migrations/0004_censorinfo_alter_movieinfo_poster_and_more.py 	new file:   movie_manager/movies/migrations/__pycache__/0004_censorinfo_alter_movieinfo_poster_and_more.cpython-312.pyc 	modified:   movie_manager/movies/models.py

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class CensorInfo(models.Model):
+    rating=models.CharField(max_length=10)
+    cirtified_by=models.CharField(max_length=200,null=True)
 class MovieInfo(models.Model):
     title=models.CharField(max_length=250)
     year=models.IntegerField(null=True)
     description=models.TextField()
     poster = models.ImageField(upload_to='images/', blank=True)  # Make sure to include 'blank=True' if the field is optional
+    censor_details=models.OneToOneField(CensorInfo,on_delete=models.SET_NULL,related_name='movie',null=True)
+    def __str__(self):
+        return self.title
 class Director(models.Model):
     name=models.CharField(max_length=300)
